412cf0c9d515530b5124e8318e08d592bc97c365
Edwin Eefting <edwin@datux.nl>
2022-03-19 19:21:06 +0100

    wip

--- reset.py
+++ reset.py
@@ -3,17 +3,5 @@ import logging
 import dobotfun
 
 robot=dobotfun.DobotFun()
-#
-# print ("Robot word gehomed")
 
-# robot.home()
-# robot.move()
-
-robot.move_to(2,100,16)
-robot.move_to(2,200,16)
-
-
-
-# while True:
-    # print (robot._get_queued_cmd_current_index())
-    # print(robot.get_pose())
+robot.home()
