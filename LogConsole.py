8ff4256fe2effc9deaecbdc81ac5c558c3e516fa
Edwin Eefting <edwin@datux.nl>
2022-03-19 19:16:09 +0100

    wip

---
+++ LogConsole.py
@@ -0,0 +1,75 @@
+# python 2 compatibility
+from __future__ import print_function
+
+import sys
+
+class LogConsole:
+    """Log-class that outputs to console, adding colors if needed"""
+
+    def __init__(self, show_debug, show_verbose, color):
+        self.last_log = ""
+        self.show_debug = show_debug
+        self.show_verbose = show_verbose
+        self._progress_uncleared=False
+
+        if color:
+            # try to use color, failback if colorama not available
+            self.colorama=False
+            try:
+                import colorama
+                global colorama
+                self.colorama = True
+            except ImportError:
+                pass
+
+        else:
+            self.colorama=False
+
+    def error(self, txt):
+        self.clear_progress()
+        if self.colorama:
+            print(colorama.Fore.RED + colorama.Style.BRIGHT + "" + txt + colorama.Style.RESET_ALL, file=sys.stderr)
+        else:
+            print("! " + txt, file=sys.stderr)
+        sys.stderr.flush()
+
+    def warning(self, txt):
+        self.clear_progress()
+        if self.colorama:
+            print(colorama.Fore.YELLOW + colorama.Style.BRIGHT + "NOTE: " + txt + colorama.Style.RESET_ALL)
+        else:
+            print("  NOTE: " + txt)
+        sys.stdout.flush()
+
+    def verbose(self, txt):
+        if self.show_verbose:
+            self.clear_progress()
+            if self.colorama:
+                print(colorama.Style.NORMAL + "" + txt + colorama.Style.RESET_ALL)
+            else:
+                print("  " + txt)
+            sys.stdout.flush()
+
+    def debug(self, txt):
+        if self.show_debug:
+            self.clear_progress()
+            if self.colorama:
+                print(colorama.Fore.GREEN + "" + txt + colorama.Style.RESET_ALL)
+            else:
+                print("# " + txt)
+            sys.stdout.flush()
+
+    def progress(self, txt):
+        """print progress output to stderr (stays on same line)"""
+        self.clear_progress()
+        self._progress_uncleared=True
+        print(">>> {}\r".format(txt), end='', file=sys.stderr)
+        sys.stderr.flush()
+
+    def clear_progress(self):
+        if self._progress_uncleared:
+            import colorama
+            print(colorama.ansi.clear_line(), end='', file=sys.stderr)
+            print(colorama.ansi.clear_line(), end='', file=sys.stdout)
+            # sys.stderr.flush()
+            self._progress_uncleared=False
---
+++ dobotfun.py
@@ -0,0 +1,84 @@
+#GPL3.0 - (c)edwin@datux.nl
+import sys
+
+from serial.tools import list_ports
+
+from LogConsole import LogConsole
+from pydobot import Dobot
+from pydobot.dobot import MODE_PTP
+
+
+class DobotFun(Dobot):
+
+    #loggen (handig als je meerdere reobots hebt)
+    def verbose(self, txt):
+        self.log.verbose(f"{self.id: <16}: {txt}")
+
+    def debug(self, txt):
+        self.log.debug(f"{self.id: <16}: {txt}")
+
+    def warning(self, txt):
+        self.log.warning(f"{self.id: <16}: {txt}")
+
+    def error(self, txt):
+        self.log.error(f"{self.id: <16}: {txt}")
+
+
+    def __init__(self, port=None, id=None):
+
+        #(dobot class logt ook nog via logger, maar deze is handiger)
+        self.log=LogConsole(show_debug=True, show_verbose=True, color=True)
+
+        if port is None:
+            self.log.verbose("Gedetecteerde poorten:")
+            for p in list_ports.comports():
+                print (" "+str(p))
+
+            self.log.verbose ("Eerste poort geselecteerd")
+            port = list_ports.comports()[0].device
+
+        if id is None:
+            self.id=port
+        self.verbose(f"Connectie maken naar poort {port} ")
+        super().__init__(port=port)
+
+        self.suck(False)
+
+    def format_pose(self):
+        p=self.get_pose()
+        return f"x={p.position.x:.2f}, y={p.position.y:.2f}, z={p.position.z:.2f}, r={p.position.r:.2f}"
+
+    def show_progress(self, extra_info=""):
+        pose=self.format_pose()
+        self.log.progress(f"[ {pose} ] {extra_info}")
+
+
+    def wacht_op(self, cmd_id):
+        """wacht op command en toon mooie progress info op beeld. ook error handeling"""
+
+        current_cmd_id = self._get_queued_cmd_current_index()
+        while cmd_id > current_cmd_id:
+            self.show_progress(f"Commando {current_cmd_id}/{cmd_id}")
+            current_cmd_id = self._get_queued_cmd_current_index()
+
+            #alarm?
+            alarms = self.get_alarms()
+            if alarms:
+                self.error(f"Alarm: {', '.join(map(str, alarms))}.")
+                sys.exit(1)
+
+
+
+
+    def home(self):
+        self.wacht_op(super().home())
+
+    def move_to(self, x, y, z, r=0., mode=MODE_PTP.MOVJ_XYZ):
+        self.debug(f"move_to x={x}, y={y}, z={z}, r={r}")
+        self.wacht_op(super().move_to(x,y,z,r,mode))
+        self.debug(self.format_pose())
+
+
+
+    def __del__(self):
+        self.verbose(f"Connectie naar robot {self.id} gesloten")
